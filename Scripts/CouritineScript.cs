7843832748b887060307c79a85b3ac81e9a3bdc6
Daan Kikkert <54845837+DaanKikkert@users.noreply.github.com>
2021-02-08 11:52:02 +0100

    Opdracht 1

---
+++ Scripts/CouritineScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CouritineScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Coroutine_");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     IEnumerator Coroutine_()
+    {
+        Debug.Log("Ik start nu de coroutine");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Coroutine update");
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Coroutine einde");
+    }
+}
